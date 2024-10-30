2b888210f4c1062c69dd52efd11bf28dba62c322
Wheslley Rimar <wheslley_rimar@hotmail.com>
2024-10-30 19:57:47 -0300

    updated comment tag

--- src/main/java/org/buildcli/utils/PomUtils.java
+++ src/main/java/org/buildcli/utils/PomUtils.java
@@ -30,7 +30,7 @@ public class PomUtils {
             }
 
             dependencyXml.append( """
-                            <--! Added by BuildCLI-->
+                            <!-- Added by BuildCLI-->
                             <dependency>
                                 <groupId>%s</groupId>
                                 <artifactId>%s</artifactId>
