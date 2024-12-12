23ae7a23b7f8dc2b14a962f1ec3a5fa9432a3de1
Wheslley Rimar <wheslley_rimar@hotmail.com>
2024-12-12 00:56:07 -0300

    Updating version

--- src/main/java/org/buildcli/BuildCLI.java
+++ src/main/java/org/buildcli/BuildCLI.java
@@ -10,7 +10,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Mixin;
 
 @Command(name = "BuildCLI", mixinStandardHelpOptions = true,
-         version = "BuildCLI 0.0.6",
+         version = "BuildCLI 0.0.8",
          description = "BuildCLI - A CLI for Java Project Management")
 public class BuildCLI implements Runnable {
 
