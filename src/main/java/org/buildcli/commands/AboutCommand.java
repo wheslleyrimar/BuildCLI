48bc624b4d2259631220b2dcb4dd4055715b7466
Dumildes Paulo <dumildesfernando@outlook.com>
2025-02-02 19:17:39 +0100

    feat: add about command

--- src/main/java/org/buildcli/commands/AboutCommand.java
+++ src/main/java/org/buildcli/commands/AboutCommand.java
@@ -1,6 +1,7 @@
 package org.buildcli.commands;
 
 import org.buildcli.domain.BuildCLICommand;
+import org.buildcli.utils.BuildCLIIntro;
 import picocli.CommandLine.Command;
 
 @Command(name = "about", aliases = {"a"}, description = "", mixinStandardHelpOptions = true)
@@ -8,6 +9,6 @@ public class AboutCommand implements BuildCLICommand {
 
   @Override
   public void run() {
-
+    BuildCLIIntro.about();
   }
 }
--- src/main/java/org/buildcli/commands/ProjectCommand.java
+++ src/main/java/org/buildcli/commands/ProjectCommand.java
@@ -1,12 +1,10 @@
 package org.buildcli.commands;
 
-import org.buildcli.commands.project.AddCommand;
-import org.buildcli.commands.project.BuildCommand;
-import org.buildcli.commands.project.RmCommand;
+import org.buildcli.commands.project.*;
 import picocli.CommandLine.Command;
 
 @Command(name = "project", aliases = {"p"}, description = "",
-    subcommands = {AddCommand.class, RmCommand.class, BuildCommand.class},
+    subcommands = {AddCommand.class, RmCommand.class, BuildCommand.class, SetCommand.class, TestCommand.class, RunCommand.class, InitCommand.class},
     mixinStandardHelpOptions = true
 )
 public class ProjectCommand {
