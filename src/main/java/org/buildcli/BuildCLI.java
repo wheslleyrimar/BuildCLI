77b6e877d738e9fbc61f56f42e1f4a77514a9037
Dumildes Paulo <dumildesfernando@outlook.com>
2025-02-02 20:22:56 +0100

    feat: add auto complete command

--- src/main/java/org/buildcli/BuildCLI.java
+++ src/main/java/org/buildcli/BuildCLI.java
@@ -1,23 +1,10 @@
 package org.buildcli;
 
-import org.buildcli.commands.AboutCommand;
-import org.buildcli.commands.ProjectCommand;
-import org.buildcli.commands.VersionCommand;
 import picocli.CommandLine.Command;
 
 @Command(name = "BuildCLI", mixinStandardHelpOptions = true,
-         version = "BuildCLI 0.0.8",
-         description = "BuildCLI - A CLI for Java Project Management")
-public class BuildCLI implements Runnable {
-
-    @Mixin
-    private OptionCommand optionCommand;
-
-    public static void main(String[] args) {
-        LoggingConfig.configure();
-
-        int exitCode = new CommandLine(new BuildCLI()).execute(args);
-        System.exit(exitCode);
-    }
+    version = "BuildCLI 0.0.8",
+    description = "BuildCLI - A CLI for Java Project Management")
+public class BuildCLI {
 
 }
