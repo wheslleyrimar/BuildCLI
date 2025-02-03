9c8616b65dd27ea9de9cc78267c0c7c458fb1fd0
Dumildes Paulo <dumildesfernando@outlook.com>
2025-02-03 14:10:04 +0100

    feat: add version command

--- src/main/java/org/buildcli/commands/VersionCommand.java
+++ src/main/java/org/buildcli/commands/VersionCommand.java
@@ -1,5 +1,6 @@
 package org.buildcli.commands;
 
+import org.buildcli.BuildCLI;
 import org.buildcli.domain.BuildCLICommand;
 import picocli.CommandLine;
 
@@ -8,6 +9,6 @@ public class VersionCommand implements BuildCLICommand {
 
   @Override
   public void run() {
-
+    new CommandLine(new BuildCLI()).execute("-V");
   }
 }
