0be18c48e3c42545f372b3f0ebefa970036feee1
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-02-09 20:30:43 -0300

    docs 📝: adds description for the “update” command to include information about updating dependencies and project settings.

--- src/main/java/org/buildcli/commands/project/UpdateCommand.java
+++ src/main/java/org/buildcli/commands/project/UpdateCommand.java
@@ -4,7 +4,8 @@ import org.buildcli.commands.project.update.DependencyCommand;
 import org.buildcli.commands.project.update.VersionCommand;
 import picocli.CommandLine.Command;
 
-@Command(name = "update", aliases = {"up"}, description = "", mixinStandardHelpOptions = true,
+@Command(name = "update", aliases = {"up"},
+ description = "Updates project dependencies and settings. Alias: 'up'. This command allows updating dependencies and configuration settings to keep the project up to date.", mixinStandardHelpOptions = true,
     subcommands = {VersionCommand.class, DependencyCommand.class}
 )
 public class UpdateCommand {
