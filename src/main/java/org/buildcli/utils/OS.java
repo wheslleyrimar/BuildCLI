29dd5e81da94ef20c91288a99a72e5424d38f7d0
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-02-16 09:39:55 -0300

    feat: created methods to move between directories, copy folder/files and get bin path

--- src/main/java/org/buildcli/utils/OS.java
+++ src/main/java/org/buildcli/utils/OS.java
@@ -1,6 +1,9 @@
 package org.buildcli.utils;
 
+import java.util.logging.Logger;
+
 public abstract class OS {
+  private static final Logger logger = Logger.getLogger(OS.class.getName());
   private OS() {}
 
   private static final String OS = System.getProperty("os.name").toLowerCase();
@@ -16,4 +19,44 @@ public abstract class OS {
   public static boolean isLinux() {
     return OS.contains("linux") || OS.contains("nix") || OS.contains("nux") || OS.contains("aix");
   }
+
+  public static void cdDirectory(String path){
+    try {
+        String[] command;
+        if (isWindows()) {
+            command = new String[]{"cmd", "/c", "cd", path};
+        } else {
+            command = new String[]{"sh", "-c", "cd", path};
+            System.out.println("cd " + path);
+        }
+        Runtime.getRuntime().exec(command);
+    } catch (Exception e) {
+      logger.severe("Error changing directory: " + e.getMessage());
+    }
+  }
+
+  public static void cpDirectory(String source, String destination){
+    try {
+      String[] command;
+      if (isWindows()) {
+        command = new String[]{"cmd", "/c", "copy", source, destination};
+      } else {
+        command = new String[]{"sh", "-c", "cp",  source, destination};
+      }
+      Runtime.getRuntime().exec(command);
+    } catch (Exception e) {
+      logger.severe("Error copying directory: " + e.getMessage());
+    }
+  }
+
+  public static String getHomeBinDirectory(){
+      String homeBin="";
+      if(isWindows()){
+          homeBin= System.getenv("HOMEPATH")+"//bin";
+      }else {
+            homeBin= System.getenv("HOME")+"/bin";
+      }
+      return homeBin;
+  }
+
 }
