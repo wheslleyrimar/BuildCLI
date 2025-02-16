ea0a9798a5f7da0cd96b81bab0b74a23167d9eb2
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-02-16 09:38:07 -0300

    feat: class for CLI Interactions such as get confirmation of an action

---
+++ src/main/java/org/buildcli/utils/tools/CLIInteractions.java
@@ -0,0 +1,21 @@
+package org.buildcli.utils.tools;
+
+import java.util.Scanner;
+
+public class CLIInteractions {
+
+    public static boolean getConfirmation(String string) {
+        String action = String.format("Do you wanna %s? (Y/N) ", string);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(action);
+        String answer = scanner.nextLine().trim().toUpperCase();
+
+        while (!answer.equals("Y") && !answer.equals("N")) {
+            System.out.println("Invalid answer. " +action);
+            answer = scanner.nextLine().trim().toUpperCase();
+        }
+
+        return answer.equalsIgnoreCase("y");
+    }
+}
